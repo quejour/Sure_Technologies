f2eac34c96a67bf51995787d6bbe87be2ea7b79f
quejour <115661210+quejour@users.noreply.github.com>
2025-06-30 13:36:47 +0100

    Added Link to Share Web Page

---
+++ src/vite.config.ts
@@ -0,0 +1,7 @@
+﻿import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  base: "/Sure_Technologies/",   // ← repo name for GitHub Pages
+  plugins: [react()],
+});
